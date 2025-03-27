4397b678ae43aaa593307ff7d6aae9fa51cb223a
Natnaelkete <ketemanatnael023@gmail.com>
2025-03-27 02:00:41 -0700

    change the logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import MobileMenu from "./MobileMenu";
+import { Droplets } from "lucide-react";
 
 export default function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -15,20 +16,8 @@ export default function Navbar() {
             {/* Logo and Brand */}
             <div className="flex items-center space-x-3">
               <Link href="/" className="flex items-center space-x-3">
-                <div className="h-9 w-9 rounded-xl bg-gradient-to-br from-sky-400 to-blue-600 flex items-center justify-center">
-                  <svg
-                    className="h-5 w-5 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
-                    />
-                  </svg>
+                <div className="h-9 w-9 rounded-xl bg-gradient-to-br from-sky-400  to-blue-600 flex items-center justify-center">
+                  <Droplets className="text-white" />
                 </div>
                 <span className="text-lg font-semibold text-white">
                   WaterWise
